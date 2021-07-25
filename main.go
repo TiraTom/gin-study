cf505b8126b4bde5dab7c4940d62e6a04fa7f20b
tiratom <217bssr@gmail.com>
2021-07-25 18:05:59 +0900

    サーバー起動

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "hello"})
+	})
+	r.Run(":8080")
+}
