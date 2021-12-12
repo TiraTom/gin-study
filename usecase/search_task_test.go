dfad4cd45e6b95b4b66ae91b7317cbe7076a200c
tiratom <217bssr@gmail.com>
2021-12-13 08:13:04 +0900

    usecaseのテストを１つ記述

--- usecase/search_task_test.go
+++ usecase/search_task_test.go
@@ -2,13 +2,13 @@ package usecase
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 
 	"github.com/Tiratom/gin-study/domain/domain_obj"
 	gr "github.com/Tiratom/gin-study/grpc"
 	"github.com/Tiratom/gin-study/mock/mock_repository_interface"
 	"github.com/golang/mock/gomock"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func TestSearchTask_Do(t *testing.T) {
@@ -16,32 +16,108 @@ func TestSearchTask_Do(t *testing.T) {
 		p *gr.GetTaskByConditionRequestParam
 	}
 	tests := []struct {
-		name            string
-		args            args
-		want            *domain_obj.Tasks
+		name string
+		args args
+		// want            *domain_obj.Tasks　　// mockからの戻り値をそのまま返すだけなのでテスト対象から外す
 		wantErr         bool
-		prepareMockFunc func() // mock準備用関数
+		prepareMockFunc func(*mock_repository_interface.MockTask) // mock準備用関数
 	}{
-		// TODO: Add test cases.
+		{
+			name: "全ての項目で検索条件を指定している場合",
+			args: args{
+				p: &gr.GetTaskByConditionRequestParam{
+					Name:                  "Name",
+					Details:               "Details",
+					ImportanceName:        "HIGH",
+					Deadline:              &timestamppb.Timestamp{Seconds: timestamp20210822150001},
+					SearchTypeForDeadline: gr.TimestampCompareBy_AFTER,
+				},
+			},
+			wantErr: false,
+			prepareMockFunc: func(mTaskR *mock_repository_interface.MockTask) {
+				mTaskR.EXPECT().Search(&domain_obj.TaskSearchCondition{
+					Name:                  "Name",
+					Details:               "Details",
+					ImportanceName:        "HIGH",
+					Deadline:              &time20210822150001,
+					SearchTypeForDeadline: &searchTypeForDeadlineAfter,
+				}).Return(&domain_obj.Tasks{
+					Value: []*domain_obj.Task{{
+						Id:             "Id",
+						Name:           "Name",
+						Details:        "Details",
+						ImportanceName: "HIGH",
+						Deadline:       &time20210822150001,
+						RegisteredAt:   &time20210822150002,
+						UpdatedAt:      &time20210822150003,
+						Version:        1,
+					}},
+				}, nil)
+			},
+		},
+		{
+			name: "検索条件が空の場合",
+			args: args{
+				p: &gr.GetTaskByConditionRequestParam{
+					Name:                  "",
+					Details:               "",
+					ImportanceName:        "",
+					Deadline:              nil,
+					SearchTypeForDeadline: searchTypeForDeadlineNone,
+				},
+			},
+			wantErr: false,
+			prepareMockFunc: func(mTaskR *mock_repository_interface.MockTask) {
+				mTaskR.EXPECT().Search(&domain_obj.TaskSearchCondition{
+					Name:                  "",
+					Details:               "",
+					ImportanceName:        "",
+					Deadline:              nil,
+					SearchTypeForDeadline: nil,
+				}).Return(&domain_obj.Tasks{
+					Value: []*domain_obj.Task{},
+				}, nil)
+			},
+		},
+		{
+			name: "検索処理でエラーが発生した場合_エラーを握りつぶしていないことを確認",
+			args: args{
+				p: &gr.GetTaskByConditionRequestParam{
+					Name:                  "Name",
+					Details:               "Details",
+					ImportanceName:        "HIGH",
+					Deadline:              &timestamppb.Timestamp{Seconds: timestamp20210822150001},
+					SearchTypeForDeadline: gr.TimestampCompareBy_AFTER,
+				},
+			},
+			wantErr: true,
+			prepareMockFunc: func(mTaskR *mock_repository_interface.MockTask) {
+				mTaskR.EXPECT().Search(&domain_obj.TaskSearchCondition{
+					Name:                  "Name",
+					Details:               "Details",
+					ImportanceName:        "HIGH",
+					Deadline:              &time20210822150001,
+					SearchTypeForDeadline: &searchTypeForDeadlineAfter,
+				}).Return(&domain_obj.Tasks{
+					Value: []*domain_obj.Task{},
+				}, fmt.Errorf("エラー"))
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			ctrl := gomock.NewController(t)
-			mtaskR := mock_repository_interface.NewMockTask(ctrl)
-			mtaskR.EXPECT().Search(tt.args).DoAndReturn(func() (*domain_obj.Tasks, error) {
-				return nil, fmt.Errorf("not yet implemented")
-			})
+			mTaskR := mock_repository_interface.NewMockTask(ctrl)
+			tt.prepareMockFunc(mTaskR)
 
 			s := &SearchTask{
-				tr: mtaskR,
+				tr: mTaskR,
 			}
-			got, err := s.Do(tt.args.p)
+
+			_, err := s.Do(tt.args.p)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("SearchTask.Do() error = %v, wantErr %v", err, tt.wantErr)
 				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("SearchTask.Do() = %v, want %v", got, tt.want)
 			}
 		})
 	}
--- usecase/values_for_test.go
+++ usecase/values_for_test.go
@@ -8,57 +8,57 @@ import (
 
 // 2021-08-21 15:00:01 UTC
 var time20210821150001 = time.Date(2021, 8, 21, 15, 0, 1, 0, time.UTC)
-var timestamp20210821150001 = 1629558001
+var timestamp20210821150001 = int64(1629558001)
 var tokyoTimeOfUtc20210821150001 = time.Date(2021, 8, 22, 0, 0, 1, 0, time.FixedZone("Asia/Tokyo", 9*60*60))
 
 // 2021/08/22 15:00:01 UTC
 var time20210822150001 = time.Date(2021, 8, 22, 15, 0, 1, 0, time.UTC)
-var timestamp20210822150001 = 1629644401
+var timestamp20210822150001 = int64(1629644401)
 var tokyoTimeOfUtc20210822150001 = time.Date(2021, 8, 23, 0, 0, 1, 0, time.FixedZone("Asia/Tokyo", 9*60*60))
 
 // 2021/08/22 15:00:02 UTC
 var time20210822150002 = time.Date(2021, 8, 22, 15, 0, 2, 0, time.UTC)
-var timestamp20210822150002 = 1629644402
+var timestamp20210822150002 = int64(1629644402)
 var tokyoTimeOfUtc20210822150002 = time.Date(2021, 8, 23, 0, 0, 2, 0, time.FixedZone("Asia/Tokyo", 9*60*60))
 
 // 2021/08/22 15:00:03 UTC
 var time20210822150003 = time.Date(2021, 8, 22, 15, 0, 3, 0, time.UTC)
-var timestamp20210822150003 = 1629644403
+var timestamp20210822150003 = int64(1629644403)
 var tokyoTimeOfUtc20210822150003 = time.Date(2021, 8, 23, 0, 0, 3, 0, time.FixedZone("Asia/Tokyo", 9*60*60))
 
 // 2021/08/22 15:00:04 UTC
 var time20210822150004 = time.Date(2021, 8, 22, 15, 0, 4, 0, time.UTC)
-var timestamp20210822150004 = 1629644404
+var timestamp20210822150004 = int64(1629644404)
 var tokyoTimeOfUtc20210822150004 = time.Date(2021, 8, 23, 0, 0, 4, 0, time.FixedZone("Asia/Tokyo", 9*60*60))
 
 // 2021/08/23 15:00:01 UTC
 var time20210823150001 = time.Date(2021, 8, 23, 15, 0, 1, 0, time.UTC)
-var timestamp20210823150001 = 1629730801
+var timestamp20210823150001 = int64(1629730801)
 var tokyoTimeOfUtc20210823150001 = time.Date(2021, 8, 24, 0, 0, 1, 0, time.FixedZone("Asia/Tokyo", 9*60*60))
 
 // 2021/08/24 15:00:01 UTC
 var time20210824150001 = time.Date(2021, 8, 24, 15, 0, 1, 0, time.UTC)
-var timestamp20210824150001 = 1629817201
+var timestamp20210824150001 = int64(1629817201)
 var tokyoTimeOfUtc20210824150001 = time.Date(2021, 8, 25, 0, 0, 1, 0, time.FixedZone("Asia/Tokyo", 9*60*60))
 
 // 2021/09/22 15:00:01 UTC
 var time20210922150001 = time.Date(2021, 9, 22, 15, 0, 1, 0, time.UTC)
-var timestamp20210922150001 = 1632322801
+var timestamp20210922150001 = int64(1632322801)
 var tokyoTimeOfUtc20210922150001 = time.Date(2021, 9, 23, 0, 0, 1, 0, time.FixedZone("Asia/Tokyo", 9*60*60))
 
 // 2021/09/22 15:00:02 UTC
 var time20210922150002 = time.Date(2021, 9, 22, 15, 0, 2, 0, time.UTC)
-var timestamp20210922150002 = 1632322802
+var timestamp20210922150002 = int64(1632322802)
 var tokyoTimeOfUtc20210922150002 = time.Date(2021, 9, 23, 0, 0, 2, 0, time.FixedZone("Asia/Tokyo", 9*60*60))
 
 // 2021/09/22 15:00:03 UTC
 var time20210922150003 = time.Date(2021, 9, 22, 15, 0, 3, 0, time.UTC)
-var timestamp20210922150003 = 1632322803
+var timestamp20210922150003 = int64(1632322803)
 var tokyoTimeOfUtc20210922150003 = time.Date(2021, 9, 23, 0, 0, 3, 0, time.FixedZone("Asia/Tokyo", 9*60*60))
 
 // 2021/09/22 15:00:04 UTC
 var time20210922150004 = time.Date(2021, 9, 22, 15, 0, 4, 0, time.UTC)
-var timestamp20210922150004 = 1632322804
+var timestamp20210922150004 = int64(1632322804)
 var tokyoTimeOfUtc20210922150004 = time.Date(2021, 9, 23, 0, 0, 4, 0, time.UTC)
 
 var searchTypeForDeadlineAfter = gr.TimestampCompareBy_AFTER
